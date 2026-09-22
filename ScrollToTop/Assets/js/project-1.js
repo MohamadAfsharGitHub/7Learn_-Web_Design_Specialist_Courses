49e8983c52cd6623aa2b0b36a22c524711277ac7
MohamadAfsharGitHub <afsharmohamademail2gmail.com>
2026-09-22 12:36:58 +0330

    update

--- ScrollToTop/Assets/js/project-1.js
+++ ScrollToTop/Assets/js/project-1.js
@@ -1,9 +1,24 @@
 
 $(window).scroll(function () {
     if ($(this).scrollTop() >=50){
-        $('#top').fadeIn();
+       // $('#top').fadeIn();
+
+       $("#top").css({
+        bottom:"20px",
+       });
+        
     }
     else {
-        $('#top').fadeOut();
+     //   $('#top').fadeOut();
+     $("#top").css({
+        bottom:"-60px",
+     });
         }
+});
+
+$('#top').click(function(e){
+    e.preventDefault();
+    $("body,html").animate({
+       scrollTop:0,
+    },1000);
 });
